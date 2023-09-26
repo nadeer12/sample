3f19fc485c56b39e6cbbf5fca8730a2aa79a25d2
Mohammmed Nadeer Kv <nadeerofficial16@gmail.com>
2023-09-26 15:21:25 +0530

    done commit

--- phishing/settings.py
+++ phishing/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#8xb2mi(xa0-x-4ki(+ub*t7*2r2&-4jwmvg%##qnj*@aw%-n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
